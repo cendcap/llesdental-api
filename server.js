30a9c5a8324916929b44f8bdb8ad19e348efc25b
John Moscol Ochoa <jmoscolo@cendcap.local>
2025-06-02 10:33:28 -0500

    🐛 Fix: forzar PORT como número en server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use('/api/usuarios', usuariosRoutes);
 app.use('/api/citas', citasRoutes);
 app.use('/api/clientes', clientesRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor backend corriendo en http://localhost:${PORT}`);
 });
